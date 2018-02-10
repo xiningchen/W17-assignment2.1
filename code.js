ec4a953b8077babfa04f15ff6eede0f5d071c014
JVLelody <xiaoxining@hotmail.com>
2018-02-09 18:23:11 -0700

    error on numChar function

--- code.js
+++ code.js
@@ -2,10 +2,15 @@
 // this is just a stub for a function you need to implement
 //
 function getStats(txt) {
+	// filter txt
+	let cleanTxt = clean(txt);
+	
+	// 
+
     return {
-        nChars: numChars(txt),
-        nWords: numWords(txt),
-        nLines: 10,
+        nChars: numChar(txt),
+        nWords: cleanTxt.length,
+        nLines: txt.split("\n").length,
         nNonEmptyLines: 22,
         averageWordLength: 3.3,
         maxLineLength: 33,
@@ -20,10 +25,35 @@ function numChar(txt){
 	return txt.length;
 }
 
-// function for coutning number of words
-function numWords(txt){
+// function for cleaning/normalizing text 
+function clean(txt){
+	let lines = txt.split("\n");
+	
+	let nonEmptyCount=0;
+	// loop through every line and clean while tracking non-empty lines
+	for(let line of lines){
+		if(line.trim().length != 0){
+			nonEmptyCount=nonEmptyCount+1;
+			
+			// since it's not empty, clean up words using regular expressions
+			
+		}
+	}
 	
 }
 
+// counts number of non-empty lines
+function nNonEmptyLines(txt){
+	let lines = txt.split("\n");
+	
+	let count=0;
+	for(let line of lines){
+		if(line.trim().length != 0){
+			count=count+1;
+		}
+	}
+	
+	return count;
+}
 
 
