ee537690e52a3e90efd99eec28cfefe30b5d3064
JVLelody <xiaoxining@hotmail.com>
2018-02-10 01:45:27 -0700

    maxlinelength added

--- code.js
+++ code.js
@@ -12,7 +12,7 @@ function getStats(txt) {
         nWords: stats.nWords,
         nLines: stats.nLines,
         nNonEmptyLines: stats.nNonEmptyLines,
-        averageWordLength: 0,
+        averageWordLength: stats.avgWordLength,
         maxLineLength: 0,
         palindromes: ["12321", "kayak", "mom"],
         longestWords: ["xxxxxxxxx", "123444444"],
@@ -32,26 +32,42 @@ function clean(txt){
 	
 	let lines = txt.split("\n");
 	let nLines = txt.split("\n").length;
+	let maxLineLength=0;
 	
 	let nNonEmptyLines=0;
 	let nWords=0;
+	let nCharWords=0;
+	
 	// loop through every line and clean while tracking non-empty lines
 	for(let line of lines){
 		if(line.trim().length != 0){
 			nNonEmptyLines+=1;
 			
+			// check line length
+			if(line.length>maxLineLength){
+				maxLineLength=line.length;
+			}
 			// since it's not empty, clean up words using regular expressions
 			line = line.replace(/\W|\_/g, " ");
-			let word = line.trim().split(" ");
-			nWords = nWords + word.length;
+			let words = line.trim().split(" ");
+			nWords += words.length;
+			
+			// process each word
+			for(let w of word){
+				nCharWords += w.length;
+			}
 		}
 	}
+	
+	let avgWordLength = nCharWords/nWords;
 	
 	return {
 		nChars, //*
 		nWords, //*
 		nLines, //*
-		nNonEmptyLines //*
+		nNonEmptyLines, //*
+		avgWordLength, //* 
+		maxLineLength //*
 	};
 }
 
